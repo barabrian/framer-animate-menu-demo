43c7e696403eed4b555a9e672f326873b44cd484
Brian <barabrian89@gmail.com>
2022-06-08 16:11:15 +0000

    font-size overwrite rules

--- src/util/cn.ts
+++ src/util/cn.ts
@@ -1,6 +1,22 @@
 import clsx, { ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+import conf from '../../tailwind.config'
+
+const tw = extendTailwindMerge({
+  classGroups: {
+    // using tailwind config to config special groups like font-size
+    'font-size': [
+      {
+        text: [
+          ...Object.keys(conf?.theme?.fontSize ?? {}),
+          ...Object.keys(conf?.theme?.extends?.fontSize ?? {}),
+        ],
+      },
+    ],
+  },
+})
 
 /** Combine clsx and twMerge */
-const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes))
+const cn = (...classes: ClassValue[]) => tw(clsx(...classes))
 export default cn
